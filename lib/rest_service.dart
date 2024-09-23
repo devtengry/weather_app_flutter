5c475adebba5f763e330a92466d9ecc046d4e42e
devtengry <imamogluomer@outlook.com>
2024-09-23 22:44:12 +0300

    pubspecfile

--- lib/rest_service.dart
+++ lib/rest_service.dart
@@ -4,6 +4,6 @@ import 'package:http/http.dart' as http;
 
 class RestService {
   Future<http.Response> fetchWeatherData() {
-    return http.get(Uri.parse('http://api.weatherapi.com/vv1'));
+    return http.get(Uri.parse('http://api.weatherapi.com/v1'));
   }
 }
